2d26f61955b8cfc0bc8c2d067fe4a1130d5106a3
cappelletto <cappelletto@gmail.com>
2020-06-30 16:50:45 +0100

    Remove loose file. Add more verbose output about TIFF metadata

--- src/gdal_test.cpp
+++ src/gdal_test.cpp
@@ -10,6 +10,8 @@
 #include <stdexcept>
 #include <vector>
 
+#include <stdio.h>
+
 using namespace std;
 
 int main()
@@ -24,6 +26,22 @@ int main()
     	return -1;
     }
 
-   	cout << "GDAL test" << endl;
+	double        adfGeoTransform[6];
+	printf( "Driver: %s/%s\n",
+	        poDataset->GetDriver()->GetDescription(),
+	        poDataset->GetDriver()->GetMetadataItem( GDAL_DMD_LONGNAME ) );
+	printf( "Size is %dx%dx%d\n",
+	        poDataset->GetRasterXSize(), poDataset->GetRasterYSize(),
+	        poDataset->GetRasterCount() );
+	if( poDataset->GetProjectionRef()  != NULL )
+	    printf( "Projection is `%s'\n", poDataset->GetProjectionRef() );
+	if( poDataset->GetGeoTransform( adfGeoTransform ) == CE_None )
+	{
+	    printf( "Origin = (%.6f,%.6f)\n",
+	            adfGeoTransform[0], adfGeoTransform[3] );
+	    printf( "Pixel Size = (%.6f,%.6f)\n",
+	            adfGeoTransform[1], adfGeoTransform[5] );
+	}
+
    	return 0;
 }
