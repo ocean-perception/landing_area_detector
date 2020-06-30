18185ddc15b3d7fc1d7413dd689ec1839b7f13ac
cappelletto <cappelletto@gmail.com>
2020-06-30 16:09:20 +0100

    Correct CMakeLists to add GDAL libraries. Create 2x target binaries for separate tests. Test reading geoTIFF

---
+++ src/gdal_test.cpp
@@ -0,0 +1,29 @@
+#include "gdal_priv.h"
+#include "cpl_conv.h" // for CPLMalloc()
+
+///Basic C and C++ libraries
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    GDALDataset  *poDataset;
+    GDALAllRegister();
+//    poDataset = (GDALDataset *) GDALOpen( pszFilename, GA_ReadOnly );
+    poDataset = (GDALDataset *) GDALOpen( "test.tif", GA_ReadOnly );
+    if( poDataset == NULL )
+    {
+    	cout << "Error when opening file" << endl;
+    	return -1;
+    }
+
+   	cout << "GDAL test" << endl;
+   	return 0;
+}
