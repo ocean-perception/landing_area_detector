8dbe3551c8cc8322c9570392642a788d006decba
cappelletto <cappelletto@gmail.com>
2020-10-22 09:49:49 +0100

    Update col def in headers.h

--- include/headers.h
+++ include/headers.h
@@ -44,6 +44,14 @@ const std::string yellow("\033[1;33m");
 const std::string blue("\033[1;34m");
 const std::string purple("\033[1;35m");
 const std::string cyan("\033[1;36m");
+
+const std::string light_red("\033[0;31m");
+const std::string light_green("\033[0;32m");
+const std::string light_yellow("\033[0;33m");
+const std::string light_blue("\033[0;34m");
+const std::string light_purple("\033[0;35m");
+const std::string light_cyan("\033[0;36m");
+
 const std::string reset("\033[0m");
 const std::string highlight("\033[30;43m");
 
